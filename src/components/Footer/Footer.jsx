e2bfa00d426ff989789ed163476ec967bbf7b210
Jordan <jwald3@brockport.edu>
2023-10-04 19:52:45 -0400

    Styled footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return (
-    <div className='main-footer'>
-        footer
-    </div>
-  )
-}
+    return (
+        <footer className="footerContainer">
+            <div className="leftSection">
+                <span>© 2023 Wattwise</span>
+            </div>
+            <div className="rightSection">
+				<Link to="/dashboard">Dashboard</Link>
+				<Link to="/privacy">Privacy</Link>
+				<Link to="/about-us">About</Link>
+				<Link to="/#">Contact Us</Link>
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
