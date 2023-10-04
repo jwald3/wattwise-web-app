fee5740bee84d4c646d6c927d7761bd9b7d47967
Jordan <jwald3@brockport.edu>
2023-10-04 00:35:52 -0400

    created filler tile baseline

---
+++ src/components/FillerPricingTierTile/FillerPricingTierTile.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./FillerPricingTierTile.css"
+
+const FillerPricingTierTile = ({ pricingTierName }) => {
+    const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    
+    return (
+        <div className="tile">
+            <div className="tile-header">
+                <div>{pricingTierName}</div>
+                <div></div>
+            </div>
+            <div className="tile-content">
+                <div className="time">
+                    "--:-- --" - "--:-- --"
+                </div>
+                <div className="days">
+                    {weekDays.map((day, index) => (
+                        <div
+                            key={day}
+                            className="day"
+                        >
+                            {day}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FillerPricingTierTile;
