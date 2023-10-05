3996cf9c699645fd4c6573a8658763fbbb035b1d
Jordan <jwald3@brockport.edu>
2023-10-04 22:09:08 -0400

    set max height constraint on header and footer

--- src/pages/PricingTierForm/PricingTierForm.jsx
+++ src/pages/PricingTierForm/PricingTierForm.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import Layout from '../../Layouts/Layout'
+import React, { useState } from 'react';
+import Layout from '../../Layouts/Layout';
 
 const PricingTierForm = () => {
+  const [formData, setFormData] = useState({
+    provider_id: "",  // Set default values as needed
+    region_id: "",
+    pricing_tier_name: "",
+    rate: "",
+    start_time: "",
+    end_time: "",
+    pricingTierDays: [],
+    pricingTierSpecialDates: [],
+  });
+
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
   return (
     <Layout>
-      <div>
-        Sample
-      </div>
+      <div>Hi</div>
     </Layout>
-  )
-}
+  );
+};
 
-export default PricingTierForm
+export default PricingTierForm;
