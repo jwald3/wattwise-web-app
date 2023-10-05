9297167bc31befed67a73bf7c7c6b8e6a655db3f
Jordan <jwald3@brockport.edu>
2023-10-04 21:30:01 -0400

    restyled footer to match header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,17 +4,19 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <footer className="footerContainer">
-            <div className="leftSection">
-                <span>© 2023 Wattwise</span>
+        <div className="footer-container">
+            <div className="main-footer">
+                <div className="leftSection">
+                    <span>© 2023 Wattwise</span>
+                </div>
+                <div className="rightSection">
+                    <Link to="/dashboard">Dashboard</Link>
+                    <Link to="/privacy">Privacy</Link>
+                    <Link to="/about-us">About</Link>
+                    <Link to="/#">Contact Us</Link>
+                </div>
             </div>
-            <div className="rightSection">
-				<Link to="/dashboard">Dashboard</Link>
-				<Link to="/privacy">Privacy</Link>
-				<Link to="/about-us">About</Link>
-				<Link to="/#">Contact Us</Link>
-            </div>
-        </footer>
+        </div>
     );
 };
 
