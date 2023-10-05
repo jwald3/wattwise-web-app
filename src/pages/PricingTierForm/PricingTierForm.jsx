1359a75d975b15f092de05b5fcd502651c6938b9
Jordan <jwald3@brockport.edu>
2023-10-05 12:04:49 -0400

    Added cancel button to tier form

--- src/pages/PricingTierForm/PricingTierForm.jsx
+++ src/pages/PricingTierForm/PricingTierForm.jsx
@@ -5,6 +5,7 @@ import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import TextField from "@mui/material/TextField";
+import { Link } from "react-router-dom";
 
 const PricingTierForm = () => {
     const [startTime, setStartTime] = useState("");
@@ -32,15 +33,14 @@ const PricingTierForm = () => {
 
     // days of the week as an array of objects, using single letter abbreviations and 1-indexed days
     const daysOfWeek = [
-		{ display: "S", value: 1}, 
-		{ display: "M", value: 2},
-		{ display: "T", value: 3},
-		{ display: "W", value: 4},
-		{ display: "T", value: 5},
-		{ display: "F", value: 6},
-		{ display: "S", value: 7},
-	];
-
+        { display: "S", value: 1 },
+        { display: "M", value: 2 },
+        { display: "T", value: 3 },
+        { display: "W", value: 4 },
+        { display: "T", value: 5 },
+        { display: "F", value: 6 },
+        { display: "S", value: 7 },
+    ];
 
     return (
         <Layout>
@@ -65,7 +65,9 @@ const PricingTierForm = () => {
                         />
                     </div>
                     <div className="input-group">
-                        <label className="dow-label" htmlFor="days">Days</label>
+                        <label className="dow-label" htmlFor="days">
+                            Days
+                        </label>
                         <div className="dow-container">
                             {daysOfWeek.map((day, i) => {
                                 return (
@@ -84,9 +86,14 @@ const PricingTierForm = () => {
                             })}
                         </div>
                     </div>
-                    <button type="submit" className="submit-button">
-                        Submit
-                    </button>
+                    <div className="button-container">
+                        <button type="submit" className="submit-button">
+                            Submit
+                        </button>
+                        <button className="cancel-button">
+                            <Link to="/dashboard">Cancel</Link>
+                        </button>
+                    </div>
                 </form>
             </div>
         </Layout>
