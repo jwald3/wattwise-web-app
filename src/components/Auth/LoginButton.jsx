388c0b9f9b0d5151da2555b20ae514b0a66af4dc
Jordan <jwald3@brockport.edu>
2023-10-10 15:22:59 -0400

    updated login button

--- src/components/Auth/LoginButton.jsx
+++ src/components/Auth/LoginButton.jsx
@@ -2,7 +2,14 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 function LoginButton() {
     const { loginWithRedirect } = useAuth0();
-    return <button onClick={() => loginWithRedirect()}>Login</button>;
+
+    const handleClick = () => {
+        loginWithRedirect({
+            appState: { targetUrl: window.location.pathname }
+        });
+    };
+
+    return <button onClick={handleClick}>Login</button>;
 }
 
 export default LoginButton;
