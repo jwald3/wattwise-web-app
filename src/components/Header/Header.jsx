12f3fe123e081886617b070e5e4d256fd39bee64
Jordan <jwald3@brockport.edu>
2023-10-10 20:49:50 -0400

    Added MUI header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,63 +1,76 @@
-import React from "react";
-import "./Header.css";
-import { Link } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
-import HeaderDropdown from "../HeaderDropdown/HeaderDropdown";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+import './Header.css';
 
 const Header = () => {
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+    
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
         <div className="header-container">
             <div className="main-header">
                 <div className="main-header-left">
-                    <div className="header-logo">
-                        <Link to="/">
-                            <img
-                                src={process.env.PUBLIC_URL + "/headerLogo.png"}
-                                alt="logo"
-                                className="header-logo-img"
-                            />
-                        </Link>
-                    </div>
+				<div className="header-logo">
+					<Link to="/">
+						<img
+							src={process.env.PUBLIC_URL + "/headerLogo.png"}
+							alt="logo"
+							className="header-logo-img"
+						/>
+					</Link>
+				</div>
                 </div>
                 <div className="main-header-right">
-                    <div className="desktop-menu">
+                    <Button onClick={handleClick} style={{ fontSize: 22}}>
+                        ...
+                    </Button>
+                    <Menu
+					anchorEl={anchorEl}
+					open={Boolean(anchorEl)}
+					onClose={handleClose}
+					PaperProps={{
+						style: {
+							maxHeight: '200px',
+							width: '20ch',
+							backgroundColor: '#F5F5F5',
+						},
+					}}
+                    >
                         {isAuthenticated ? (
                             <>
-                                <Link to="/dashboard">Dashboard</Link>
-                                <Link to="/privacy">Privacy</Link>
-                                <Link to="/about-us">About</Link>
-                                <Link to="https://twitter.com/WattwiseApp">
-                                    Contact Us
-                                </Link>
-                                <div
-                                    onClick={() =>
-                                        logout({
-                                            returnTo: window.location.origin,
-                                        })
-                                    }
-                                    style={{ cursor: "pointer" }}
-                                >
+                                <MenuItem onClick={handleClose}><Link to="/dashboard">Dashboard</Link></MenuItem>
+                                <MenuItem onClick={handleClose}><Link to="/privacy">Privacy</Link></MenuItem>
+                                <MenuItem onClick={handleClose}><Link to="/about-us">About</Link></MenuItem>
+                                <MenuItem onClick={handleClose}><Link to="https://twitter.com/WattwiseApp">Contact Us</Link></MenuItem>
+                                <MenuItem onClick={() => {
+                                    logout({ returnTo: window.location.origin });
+                                    handleClose();
+                                }}>
                                     Logout
-                                </div>
+                                </MenuItem>
                             </>
                         ) : (
-                            <div
-                                onClick={loginWithRedirect}
-                                style={{ cursor: "pointer" }}
-                            >
+                            <MenuItem onClick={() => {
+                                loginWithRedirect();
+                                handleClose();
+                            }}>
                                 Login
-                            </div>
+                            </MenuItem>
                         )}
-                    </div>
-                    <div className="mobile-menu">
-                        <HeaderDropdown
-                            isAuthenticated={isAuthenticated}
-                            loginWithRedirect={loginWithRedirect}
-                            logout={logout}
-                        />
-                    </div>
+                    </Menu>
                 </div>
             </div>
         </div>
